927bd01a89d2a531e2a0d81e24c2260e16f9f823
joaofidelisc <joao.fidelis@estudante.ufscar.br>
2022-07-17 23:43:53 -0300

    dependencias de navegacao atualizadas

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-import Login from './src/screens/Login';
+const Tab = createBottomTabNavigator();
 
-
-function App() {
-  return(
-    <Login/>
-  )
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
 }
 
-export default App;
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
