1339237326637ae3b8591bbd0452ed28f268b7f1
joaofidelisc <joao.fidelis@estudante.ufscar.br>
2022-07-17 22:23:46 -0300

    sign with email/google

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import {
-  SafeAreaView, 
+  SafeAreaView,
   StatusBar,
-  View,
+  StyleSheet,
   Text,
+  View,
   TextInput,
-  TouchableOpacity
 } from 'react-native';
+
 
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth'
@@ -18,20 +19,24 @@ GoogleSignin.configure({
   webClientId: '97527742455-7gie5tgugbocjpr1m0ob9sdua49au1al.apps.googleusercontent.com',
 });
 
-function App() {
-
+function Login(){
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const SignInGoogle = async() =>{
+    // Get the users ID token
     const { idToken } = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
     res = await auth().signInWithCredential(googleCredential);
-    console.log(res);
   }
 
   const SignOutGoogle = async() =>{
     try {
       await GoogleSignin.signOut();
+      // this.setState({ user: null }); // Remember to remove the user from your app's state as well
     } catch (error) {
       console.error(error);
     }
@@ -43,21 +48,27 @@ function App() {
       console.log(dominios_permitidos.includes(dominio))
       alert('Apenas dominínios universitários são permitidos')
     }
-    auth().createUserWithEmailAndPassword(email, password).then(() => {
+    auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => {
       console.log('User account created & signed in!');
       //implementar alguma coisa aqui
-    }).catch(error => {
+    })
+    .catch(error => {
       if (error.code === 'auth/email-already-in-use') {
         console.log('That email address is already in use!');
         //implementar alguma coisa aqui
       }
+
       if (error.code === 'auth/invalid-email') {
         console.log('That email address is invalid!');
         //implementar alguma coisa aqui
       }
+
       console.error(error);
     });
   }
+
   return (
     <SafeAreaView>
       <StatusBar barStyle={'light-content'} />
@@ -67,30 +78,24 @@ function App() {
             style={{backgroundColor: '#C4C4C4', width: 291, height: 47, top: 167, alignSelf: 'center', borderRadius: 15, fontWeight: '400', fontSize: 18, lineHeight: 22}}
             placeholder='E-mail'
             keyboardType='email-address'
-            onChangeText={(email)=>setEmail(email)}
-            />
+          />
           <TextInput
             style={{backgroundColor: '#C4C4C4', width: 291, height: 47, top: 200, alignSelf: 'center', borderRadius: 15, fontWeight: '400', fontSize: 18, lineHeight: 22}}
             placeholder='Senha'
             secureTextEntry={true}
-            onChangeText={(password)=>setPassword(password)}
           />
-          <TouchableOpacity 
-            style={{position: 'absolute', width: 291, height: 47, top: 395, backgroundColor: '#C4C4C4', borderRadius: 15, alignSelf: 'center', justifyContent: 'center'}}
-            onPress={SignInGoogle}
-          >
-            
+          <TouchableOpacity style={{position: 'absolute', width: 291, height: 47, top: 395, backgroundColor: '#C4C4C4', borderRadius: 15, alignSelf: 'center', justifyContent: 'center'}}>
             <Text style={{fontWeight: '400', fontSize: 16, lineHeight: 20, textAlign: 'center'}}>Continuar com Google</Text>
           </TouchableOpacity>
-          <TouchableOpacity 
-            style={{position: 'absolute', width: 291, height: 47, top: 492, backgroundColor: '#FF5F55', borderRadius: 15, alignSelf: 'center', justifyContent: 'center'}}
-            onPress={InsertUserWithEmail}
-            >
+          <TouchableOpacity style={{position: 'absolute', width: 291, height: 47, top: 492, backgroundColor: '#FF5F55', borderRadius: 15, alignSelf: 'center', justifyContent: 'center'}}>
             <Text style={{color: 'white', fontWeight: '600', fontSize: 20, lineHeight: 24, textAlign: 'center'}}>Continuar</Text>
           </TouchableOpacity>
         </View>
-      </SafeAreaView>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default App;
+const styles = StyleSheet.create({
+});
+
+export default Login;
