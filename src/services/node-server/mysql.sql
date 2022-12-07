eeffddf921b0fb6ae195392da25b576a037f98f2
joaofidelisc <joao.fidelis@estudante.ufscar.br>
2022-12-07 08:54:20 -0300

    atualizações - modelo banco de dados

--- src/services/node-server/mysql.sql
+++ src/services/node-server/mysql.sql
@@ -1,7 +1,20 @@
 CREATE DATABASE IF NOT EXISTS caronasuniversitarias;
 SHOW DATABASES;
-CREATE TABLE Users(
-	User_ID INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+USE caronasuniversitarias;
+
+/*
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Veiculo;
+DROP TABLE IF EXISTS Viagens;
+*/
+
+-- nomeCliente
+-- nome_cliente
+
+
+
+CREATE TABLE IF NOT EXISTS User(
+	User_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR (150) NOT NULL,
     CPF VARCHAR (14) NOT NULL,
     data_nasc DATE,
@@ -10,25 +23,33 @@ CREATE TABLE Users(
     token VARCHAR (100) NOT NULL,
     universidade VARCHAR (100) NOT NULL,
     classificacao FLOAT,
-    numViagensRealizadas INT NOT NULL,
+    -- numViagensRealizadas INT NOT NULL,
+    fotoPerfil VARCHAR (100) NOT NULL,
     motorista BOOLEAN
 );
 
-CREATE TABLE Veiculo(
-	Veiculo_ID VARCHAR (150) NOT NULL, 
-    User_ID INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	nome_veiculo VARCHAR (150) NOT NULL,
-    ano_veiculo INT,
-    cor_veiculo VARCHAR (15) NOT NULL,
-    placa_veiculo VARCHAR (7) NOT NULL,
-);
+-- CREATE TABLE IF NOT EXISTS Veiculo(
+-- 	Veiculo_ID INTEGER PRIMARY KEY AUTO_INCREMENT, 
+-- 	nome_veiculo VARCHAR (150) NOT NULL,
+--     ano_veiculo INT NOT NULL,
+--     cor_veiculo VARCHAR (15) NOT NULL,
+--     placa_veiculo VARCHAR (7) NOT NULL,
+--     -- FOTOVEICULO
+--     CONSTRAINT fk_User_ID FOREIGN KEY (User_ID) REFERENCES User (User_ID)
+-- );
 
-CREATE TABLE Viagens(
-	User_ID INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    refViagem VARCHAR (150) NOT NULL,
-    uidMotorista VARCHAR (150) NOT NULL,
-	dataViagem DATE,
-    destino VARCHAR (150) NOT NULL,
-    fotoPerfil IMAGE,
-    nome VARCHAR (150) NOT NULL,
-);
+
+-- -- RELACIONAMENTO N P/ N
+-- -- CRIAR UMA TABELA A MAIS
+-- CREATE TABLE IF NOT EXISTS Viagem(
+--     ref_viagem INTEGER PRIMARY KEY AUTO_INCREMENT,
+-- 	dataViagem DATE,
+--     destino VARCHAR (150) NOT NULL,
+--     -- fotoPerfil IMAGE,
+--     -- fotoPerfil VARCHAR (100) NOT NULL,
+--     -- nome VARCHAR (150) NOT NULL,
+--     uidMotorista VARCHAR (150) NOT NULL, --CHAVE ESTRANGEIRA;
+--     CONSTRAINT fk_User_ID FOREIGN KEY (User_ID) REFERENCES User (User_ID)
+-- );
+
+SELECT * FROM Users;
