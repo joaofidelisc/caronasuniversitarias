d2b21d086e38283227b35526269742083a51a269
GuiEscher <guilhermeeschersalvador@gmail.com>
2022-12-06 22:43:47 -0300

    Tipagem - mysql

--- src/services/node-server/mysql.sql
+++ src/services/node-server/mysql.sql
@@ -15,20 +15,20 @@ CREATE TABLE Users(
 );
 
 CREATE TABLE Veiculo(
-	Veiculo_ID,
-    User_ID,
-	nome_veiculo,
-    ano_veiculo,
-    cor_veiculo,
-    placa_veiculo,
+	Veiculo_ID VARCHAR (150) NOT NULL, 
+    User_ID INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	nome_veiculo VARCHAR (150) NOT NULL,
+    ano_veiculo INT,
+    cor_veiculo VARCHAR (15) NOT NULL,
+    placa_veiculo VARCHAR (7) NOT NULL,
 );
 
 CREATE TABLE Viagens(
-	User_ID,
-    refViagem,
-    uidMotorista,
-	dataViagem,
-    destino,
-    fotoPerfil,
-    nome,
+	User_ID INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    refViagem VARCHAR (150) NOT NULL,
+    uidMotorista VARCHAR (150) NOT NULL,
+	dataViagem DATE,
+    destino VARCHAR (150) NOT NULL,
+    fotoPerfil IMAGE,
+    nome VARCHAR (150) NOT NULL,
 );
