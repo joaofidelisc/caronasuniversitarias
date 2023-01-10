5866054fd95216bbdb4b439e99d5282973858b34
joaofidelisc <joao.fidelis@estudante.ufscar.br>
2023-01-09 23:37:54 -0300

    modelo viagem criado

---
+++ migrations/20230110022757-create-viagem.js
@@ -0,0 +1,87 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Viagem', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING(100)
+      },
+      uidPassageiro1: {
+        allowNull: true,
+        type: Sequelize.STRING(50),
+        references:{
+          model: 'user',
+          key: 'id'
+        },
+        onUpdate: 'NO ACTION',
+        onDelete: 'NO ACTION'
+      },
+      uidPassageiro2: {
+        allowNull: true,
+        type: Sequelize.STRING(50),
+        references:{
+          model: 'user',
+          key: 'id'
+        },
+        onUpdate: 'NO ACTION',
+        onDelete: 'NO ACTION'
+      },
+      uidPassageiro3: {
+        allowNull: true,
+        type: Sequelize.STRING(50),
+        references:{
+          model: 'user',
+          key: 'id'
+        },
+        onUpdate: 'NO ACTION',
+        onDelete: 'NO ACTION'
+      },
+      uidPassageiro4: {
+        allowNull: true,
+        type: Sequelize.STRING(50),
+        references:{
+          model: 'user',
+          key: 'id'
+        },
+        onUpdate: 'NO ACTION',
+        onDelete: 'NO ACTION'
+      },
+      uidMotorista: {
+        allowNull: true,
+        type: Sequelize.STRING(50),
+        references:{
+          model: 'user',
+          key: 'id'
+        },
+        onUpdate: 'NO ACTION',
+        onDelete: 'NO ACTION'
+      },
+      fotoPerfil: {
+        type: Sequelize.STRING
+      },
+      destino: {
+        type: Sequelize.STRING
+      },
+      dataViagem: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Viagem');
+  }
+};
---
+++ models/viagem.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Viagem extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Viagem.belongsTo(models.User);
+    }
+  }
+  Viagem.init({
+    nomeMotorista: DataTypes.STRING(100),
+    uidPassageiro1: DataTypes.STRING(50),
+    uidPassageiro2: DataTypes.STRING(50),
+    uidPassageiro3: DataTypes.STRING(50),
+    uidPassageiro4: DataTypes.STRING(50),
+    uidMotorista: DataTypes.STRING(50),
+    fotoPerfilMotorista: DataTypes.STRING,
+    destino: DataTypes.STRING,
+    dataViagem: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Viagem',
+    tableName: 'Viagem'
+  });
+  return Viagem;
+};
