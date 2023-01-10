0aa2b4efb5e2921b662e9263c34fc9f403867d57
GuiEscher <guilhermeeschersalvador@gmail.com>
2023-01-09 23:19:17 -0300

    Inicio controller de viagem

--- src/services/controllers/viagem.js
+++ src/services/controllers/viagem.js
@@ -13,3 +13,34 @@
 // ATUALIZAR HISTÓRICO DE VIAGENS MOTORISTA(
 //     ?
 // )
+
+
+const model = require('../../../models');
+
+async function getUidMotorista(req, res){
+    let reqs = await model.viagem.findAll({
+        where: {
+            userId: req.params.userId
+        }
+    });
+    if (reqs){
+        res.send(JSON.stringify(reqs));
+    }else{
+        //RESPOSTA AO FRONT-END AQUI CASO DÊ ERRADO!
+        res.send(JSON.stringify('Falha'));
+    }        
+}
+
+async function getDataViagem(req, res){
+    let reqs = await model.viagem.findAll({
+        where: {
+            userId: req.params.dataViagem
+        }
+    });
+    if (reqs){
+        res.send(JSON.stringify(reqs));
+    }else{
+        //RESPOSTA AO FRONT-END AQUI CASO DÊ ERRADO!
+        res.send(JSON.stringify('Falha'));
+    }        
+}
